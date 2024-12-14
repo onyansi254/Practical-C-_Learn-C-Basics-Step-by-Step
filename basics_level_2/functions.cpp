f145a0391047eab1d4d424bfe5504113a3ea4978
Rihard Otwoma <ronyansi11@gmail.com>
2024-12-14 23:16:47 +0300

    Functions can be used as many times as possible

---
+++ basics_level_2/functions.cpp
@@ -0,0 +1,15 @@
+// function is a bloc of code that you can reuse later as many as you want
+
+#include <iostream>
+
+void say_hello()
+{
+    std::cout << "Hello" << std::endl;
+    std::cout << "Richard" << std::endl;
+}
+
+int main()
+{
+    say_hello();
+    return 0;
+}
