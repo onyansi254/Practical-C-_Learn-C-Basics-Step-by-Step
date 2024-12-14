372b0167b59b769c10c8253a8d1e1ef00c71266e
Rihard Otwoma <ronyansi11@gmail.com>
2024-12-14 23:34:00 +0300

    Use of parameters in functions to make code more easier and i be more lazy

---
+++ basics_level_2/functinos_parameters.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+void my_self(std::string user_name, int user_age, std::string mood)
+{
+    std::cout << "Hello" << user_name << "you are " << user_age<< " " << "and " << mood <<std::endl;
+}
+
+int main()
+{
+    my_self("Richard", 33, "confused");
+    my_self("Onyansi", 20, "lost");
+
+    return 0;
+}
