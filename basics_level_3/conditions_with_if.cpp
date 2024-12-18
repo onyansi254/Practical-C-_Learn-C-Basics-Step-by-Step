9a36708b50d6bb47b9d83f61b455a8fc8b7c9813
Rihard Otwoma <ronyansi11@gmail.com>
2024-12-18 16:25:08 +0300

    conditions  with if

---
+++ basics_level_3/conditions_with_if.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int user_age = 27;
+
+    if (user_age >= 18) {
+        cout << "You are officially an adult." << endl;
+    }
+
+    cout << "End of program." << endl;
+    return 0;
+}
