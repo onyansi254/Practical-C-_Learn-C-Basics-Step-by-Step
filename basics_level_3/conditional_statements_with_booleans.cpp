fa4d6ede3959a870fe6806185f8dbca86584f0a1
Rihard Otwoma <ronyansi11@gmail.com>
2024-12-18 16:04:47 +0300

    Conditional statements

---
+++ basics_level_3/conditional_statements_with_booleans.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << boolalpha;   // when used the boolean values are represented as text not as (1 or 0)
+    
+    cout << (1==1) << endl;  //for comparison operators !=, ==, <, <=, >, >=
+
+    //cout << true << endl;
+    //cout << false << endl;
+
+    cout << ("hello" == "hallo") << endl;
+    return 0;
+}
