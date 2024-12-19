ad6566389060f90a0e22ecc65ddf7c21f87e22be
Rihard Otwoma <ronyansi11@gmail.com>
2024-12-19 10:57:06 +0300

    While loop

---
+++ basics_level_3/while_loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // for (int i=0; i<10; i++){
+    //     cout << "Hello thanks." << endl;
+    // }
+
+    int i = 0;
+    while (i < 10) {
+        cout << "Hello " << i << endl;
+        i++;
+    }
+
+    return 0;
+}
